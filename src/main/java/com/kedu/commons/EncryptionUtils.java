2462dd6fd463fef47a437876eb0210ad7fb7dfce
하늘 <skym9753@gamil.com>
2026-03-26 12:29:57 +0900

    new Test01_Board create

---
+++ src/main/java/com/kedu/commons/EncryptionUtils.java
@@ -0,0 +1,24 @@
+package com.kedu.commons;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class EncryptionUtils {
+   public static String getSha512(String text) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            byte[] bytes = text.getBytes(StandardCharsets.UTF_8);
+            byte[] digest = md.digest(bytes);
+            
+            StringBuilder builder = new StringBuilder();
+            for (byte b : digest) {
+                builder.append(String.format("%02x", b));
+            }
+            return builder.toString();
+            
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("SHA-512 암호화 실패", e);
+        }
+    }
+}
